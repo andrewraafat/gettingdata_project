695b0c45cc2de639e062e54e7a9cfab0c03daed7
Andrew Raafat <andrew.raafat.404@gmail.com>
2014-07-27 22:25:04 +0200

    script updated and code book

---
+++ CodeBook.md
@@ -0,0 +1,5 @@
+###The New variables
+
+*SubjectID: indicates the Identifier of the expermint subject. 
+*Activity : indicates the activity type done
+*FeaturesVector: indicates the vector of features descriped in the features.txt file
--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,21 @@
-trainx <- read.table("UCI HAR Dataset/train/X_train.txt", sep = " ", fill = T)
+ActLab <- read.table("UCI HAR Dataset/activity_labels.txt")
 
+trainx <- read.table("UCI HAR Dataset/train/X_train.txt", sep = "\t")
+name(trainx) <- "FeaturesVector"
 trainy <- read.table("UCI HAR Dataset/train/y_train.txt", sep = " ")
-
+name(trainy) <- "Activity"
 trainsub <- read.table("UCI HAR Dataset/train/subject_train.txt", sep = " ")
+name(trainy) <- "SubjectID"
 
 
-testx <- read.table("UCI HAR Dataset/test/X_test.txt", sep = " ", fill = T)
-
+testx <- read.table("UCI HAR Dataset/test/X_test.txt", sep = "\t")
+name(testx) <- "FeaturesVector"
 testy <- read.table("UCI HAR Dataset/test/y_test.txt", sep = " ")
-
+name(testy) <- "Activity"
 testsub <- read.table("UCI HAR Dataset/test/subject_test.txt", sep = " ")
+name(testsub) <- "SubjectID"
+
+
+finalx <- rbind(trainx,testx)
+finaly <- rbind(trainy,testy)
+finalsub <- rbind(trainsub,testsub)
